62e3e66e6e1c7b5977d2c67de923c141d6e34fdf
chenxinyu <chenxinyu@zhuanzhuan.com>
2022-07-26 15:15:23 +0800

    [dev] 修改调度中心地址

--- src/views/basicmain.vue
+++ src/views/basicmain.vue
@@ -9,7 +9,8 @@
 				<el-menu-item index="/itvmain">面试管理</el-menu-item>
 			</el-submenu>
 			<el-menu-item index="/welcome">分享平台</el-menu-item>
-			<el-menu-item v-if="userRole == 1" index="/dbmain">数据基础平台</el-menu-item>
+			<el-menu-item v-if="userRole == 1" index="/dbmain">数据管理</el-menu-item>
+			<el-menu-item v-if="userRole == 1" @click="toScheduing()">任务调度</el-menu-item>
 			<el-tooltip class="item" content="退出登录" effect="dark" placement="bottom">
 				<el-menu-item @click="logOut">退出</el-menu-item>
 			</el-tooltip>
@@ -22,26 +23,28 @@
 	</div>
 </template>
 <script>
-	export default {
-		data() {
-			return {
-				activeIndex: '1',
-				activeIndex2: '1',
-				userRole: sessionStorage.getItem("userRole")
-			};
+export default {
+	data() {
+		return {
+			activeIndex: '1',
+			activeIndex2: '1',
+			userRole: sessionStorage.getItem("userRole")
+		};
+	},
+	methods: {
+		handleSelect(key, keyPath) {
+			console.log(key, keyPath);
 		},
-		methods: {
-			handleSelect(key, keyPath) {
-				console.log(key, keyPath);
-			},
-			logOut() {
-				sessionStorage.clear();
-				this.$router.push("/login");
-			}
+		logOut() {
+			sessionStorage.clear();
+			this.$router.push("/login");
+		},
+		toScheduing() {
+			window.open("http://124.222.34.234:27316/xxl-job-admin/")
 		}
 	}
+}
 </script>
 
 <style>
-
 </style>
